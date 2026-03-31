65ad24a42c753e9c8e457901f60547c19c37cfca
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2026-03-31 09:02:17 +0600

    Add files via upload

---
+++ 01_FileCreation/01_Basic.sh
@@ -0,0 +1,5 @@
+#! /bin/bash  
+# Here First two characters are called 'shebang' and it tells the system which interpreter to use to execute the script. In this case, it specifies that the script should be run using the Bash shell.
+# After shebang the path of the interpreter is given which we found by using 'which' command in terminal.
+
+echo "Hello World"
