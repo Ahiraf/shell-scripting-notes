74d00f5b396ed0df3b2a36ecbf3b80257689aafc
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2026-04-01 22:26:02 +0600

    Create main.sh

---
+++ 02_variables-&-comments/main.sh
@@ -0,0 +1,17 @@
+# This is comment.
+# Two types of  variables : 1) System variables 2) User defined variables
+
+# System variables : These are predfined variables that are set by the operating system or shell. They provide information about the system and environment. 
+
+echo Our shell name is $BASH
+echo Our shell version name is $BASH_VERSION
+echo Our Home directory is $HOME
+echo Our current working directory is $PWD
+
+name=Fariha
+10val=10
+echo The name is $name
+echo value $10val # This will give an error because variable names cannot start with a number.
+
+val=10
+echo the value is $val
