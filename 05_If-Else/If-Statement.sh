7e0801308b4128301e7b88ec60d8ca0c07e5aad6
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2026-04-18 00:07:53 +0600

    Update If-Statement.sh

--- 05_If-Else/If-Statement.sh
+++ 05_If-Else/If-Statement.sh
@@ -1,1 +1,33 @@
+#! /bin/bash
+
+count=10
+#if [ $count -eq 10 ]
+#then 
+   #echo "The statement is true"
+#fi
+
+#if [ $count -ne 9 ]
+#then 
+    #echo "The statement is true"
+#fi
+
+
+if [ $count > 9 ]
+then 
+    echo "The statement is true"
+fi
+
+#or
+if (( $count > 9 ))
+then 
+    echo "The statement is true"
+fi
+
+#In case of string : 
+
+word=abc
+if [ $word == "abc" ]  # or $word = "abc"
+then 
+    echo "The statement is true"
+fi
 
